7699fd6f5e72a8697f98c74626d368071e58956a
RodelinDev <rdbivieca@gmail.com>
2025-05-12 01:01:40 -0400

    Update navigation routes.

--- app/src/main/java/com/rodelindev/tvshows/navigation/NavigationHost.kt
+++ app/src/main/java/com/rodelindev/tvshows/navigation/NavigationHost.kt
@@ -7,6 +7,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.rodelindev.tvshows.navigation.NavigationRoute.Detail
+import com.rodelindev.tvshows.navigation.NavigationRoute.Home
+import com.rodelindev.tvshows.navigation.NavigationRoute.Profile
+import com.rodelindev.tvshows.navigation.NavigationRoute.Splash
 import com.rodelindev.tvshows.presentation.detail.DetailScreen
 import com.rodelindev.tvshows.presentation.home.HomeScreen
 import com.rodelindev.tvshows.presentation.profile.ProfileScreen
@@ -18,18 +22,18 @@ fun NavigationHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = NavigationRoute.Splash,
+        startDestination = Splash,
         enterTransition = { slideInHorizontally { it } },
         exitTransition = { slideOutHorizontally { - it } },
         popEnterTransition = { slideInHorizontally { - it } },
         popExitTransition = { slideOutHorizontally { it } }
     ) {
-        composable<NavigationRoute.Splash>(
+        composable<Splash>(
             enterTransition = { slideInVertically() }
         ) {
             SplashScreen(
                 navigateToHome = {
-                    navController.navigate(NavigationRoute.Home) {
+                    navController.navigate(Home) {
                         popUpTo(navController.graph.id) {
                             inclusive = true
                         }
@@ -38,20 +42,18 @@ fun NavigationHost(
             )
         }
 
-        composable<NavigationRoute.Home> {
+        composable<Home> {
             HomeScreen(
                 onClickItem = { tvShow ->
-                    navController.navigate(
-                        NavigationRoute.Detail(tvShow.id)
-                    )
+                    navController.navigate(Detail(tvShow.id))
                 },
                 navigateToProfile = {
-                    navController.navigate(NavigationRoute.Profile)
+                    navController.navigate(Profile)
                 }
             )
         }
 
-        composable<NavigationRoute.Detail> {
+        composable<Detail> {
             DetailScreen(
                 onBack = {
                     navController.navigateUp()
@@ -59,7 +61,7 @@ fun NavigationHost(
             )
         }
 
-        composable<NavigationRoute.Profile> {
+        composable<Profile> {
             ProfileScreen(
                 onBack = {
                     navController.popBackStack()
